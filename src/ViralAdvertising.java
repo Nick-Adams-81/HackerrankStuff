14fe093b28c8c38d478e0c2beecb5b6cfddfa2a2
Nick Adams <nick.pappas.adams@gmail.com>
2023-06-21 08:35:45 -0700

    added viral advertising

---
+++ src/ViralAdvertising.java
@@ -0,0 +1,19 @@
+public class ViralAdvertising {
+
+    public static int viralAdvertising(int n) {
+        int shared = 5;
+        int likes = 0;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            likes = shared / 2;
+            shared = likes * 3;
+            count = count + likes;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(viralAdvertising(5));
+    }
+
+}
